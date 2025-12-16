9c403e9255e0ec33956c1c24beff7d4bea928122
ekzyis <ramdip.singhgill@gmail.com>
2025-12-16 18:35:29 +0100

    Add other encode/decode tests with valid data but skip for now

--- lightning/bolt11/bolt11_test.go
+++ lightning/bolt11/bolt11_test.go
@@ -9,6 +9,7 @@ import (
 	_secp256k1 "github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4/ecdsa"
 	"github.com/ekzyis/lntutor/lib/secp256k1"
+	"github.com/ekzyis/lntutor/lightning/lntypes"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -134,6 +135,311 @@ func TestPaymentRequest_EncodeBech32_Spec_004(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(
 		"lnbc20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqhp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqs9qrsgq7ea976txfraylvgzuxs8kgcw23ezlrszfnh8r6qtfpr6cxga50aj6txm9rxrydzd06dfeawfk6swupvz4erwnyutnjq7x39ymw6j38gp7ynn44",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_005(t *testing.T) {
+	// The same, on testnet, with a fallback address mk2QpYatsKicvFVuTAQLBryyccRXMUaGHP
+
+	t.Skip("tagged field f not supported yet")
+	t.Skip("different order of tagged fields not supported yet")
+
+	assert := assert.New(t)
+
+	pr := NewPaymentRequest(
+		2_000_000_000,
+		WithNetwork(lntypes.NetworkTestnet),
+		WithTimestamp(timestamp),
+		WithDescriptionHash(longDescriptionHash),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithFallbackAddress("mk2QpYatsKicvFVuTAQLBryyccRXMUaGHP"), // P2PKH
+		WithFeatureBits(PaymentSecretRequired, VarOnionOptinRequired),
+		WithExpiry(0),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lntb20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygshp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqfpp3x9et2e20v6pu37c5d9vax37wxq72un989qrsgqdj545axuxtnfemtpwkc45hx9d2ft7x04mt8q7y6t0k2dge9e7h8kpy9p34ytyslj3yu569aalz2xdk8xkd7ltxqld94u8h2esmsmacgpghe9k8",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_006(t *testing.T) {
+	// On mainnet, with fallback address 1RustyRX2oai4EYYDpQGWvEL62BBGqN9T
+	// with extra routing info to go via nodes
+	// 029e03a901b85534ff1e92c43c74431f7ce72046060fcf7a95c37e148f78c77255
+	// then 039e03a901b85534ff1e92c43c74431f7ce72046060fcf7a95c37e148f78c77255
+
+	t.Skip("tagged fields f and r not supported yet")
+
+	assert := assert.New(t)
+
+	pr := NewPaymentRequest(
+		2_000_000_000,
+		WithTimestamp(timestamp),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithDescriptionHash(longDescriptionHash),
+		WithFallbackAddress("1RustyRX2oai4EYYDpQGWvEL62BBGqN9T"), // P2PKH
+		// TODO: add routing info (r) from test vector
+		WithFeatureBits(PaymentSecretRequired, VarOnionOptinRequired),
+		WithExpiry(0),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqhp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqsfpp3qjmp7lwpagxun9pygexvgpjdc4jdj85fr9yq20q82gphp2nflc7jtzrcazrra7wwgzxqc8u7754cdlpfrmccae92qgzqvzq2ps8pqqqqqqpqqqqq9qqqvpeuqafqxu92d8lr6fvg0r5gv0heeeqgcrqlnm6jhphu9y00rrhy4grqszsvpcgpy9qqqqqqgqqqqq7qqzq9qrsgqdfjcdk6w3ak5pca9hwfwfh63zrrz06wwfya0ydlzpgzxkn5xagsqz7x9j4jwe7yj7vaf2k9lqsdk45kts2fd0fkr28am0u4w95tt2nsq76cqw0",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_007(t *testing.T) {
+	// On mainnet, with fallback (P2SH) address
+	// 3EktnHQD7RiAE6uzMj2ZifT9YgRrkSgzQX
+
+	t.Skip("tagged field f not supported yet")
+	t.Skip("different order of tagged fields not supported yet")
+
+	assert := assert.New(t)
+
+	pr := NewPaymentRequest(
+		2_000_000_000,
+		WithTimestamp(timestamp),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithDescriptionHash(longDescriptionHash),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithExpiry(0),
+		WithFallbackAddress("3EktnHQD7RiAE6uzMj2ZifT9YgRrkSgzQX"), // P2SH
+		WithFeatureBits(PaymentSecretRequired, VarOnionOptinRequired),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygshp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqfppj3a24vwu6r8ejrss3axul8rxldph2q7z99qrsgqz6qsgww34xlatfj6e3sngrwfy3ytkt29d2qttr8qz2mnedfqysuqypgqex4haa2h8fx3wnypranf3pdwyluftwe680jjcfp438u82xqphf75ym",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_008(t *testing.T) {
+	// On mainnet, with fallback (P2WPKH) address
+	// bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4
+
+	t.Skip("tagged field f not supported yet")
+	t.Skip("different order of tagged fields not supported yet")
+
+	assert := assert.New(t)
+
+	pr := NewPaymentRequest(
+		2_000_000_000,
+		WithTimestamp(timestamp),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithDescriptionHash(longDescriptionHash),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithFallbackAddress("bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4"), // P2WPKH
+		WithFeatureBits(PaymentSecretRequired, VarOnionOptinRequired),
+		WithExpiry(0),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygshp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqfppqw508d6qejxtdg4y5r3zarvary0c5xw7k9qrsgqt29a0wturnys2hhxpner2e3plp6jyj8qx7548zr2z7ptgjjc7hljm98xhjym0dg52sdrvqamxdezkmqg4gdrvwwnf0kv2jdfnl4xatsqmrnsse",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_009(t *testing.T) {
+	// On mainnet, with fallback (P2WSH) address
+	// bc1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3qccfmv3
+
+	t.Skip("tagged field f not supported yet")
+	t.Skip("different order of tagged fields not supported yet")
+
+	assert := assert.New(t)
+
+	pr := NewPaymentRequest(
+		2_000_000_000,
+		WithTimestamp(timestamp),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithDescriptionHash(longDescriptionHash),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithFallbackAddress("bc1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3qccfmv3"), // P2WSH
+		WithFeatureBits(PaymentSecretRequired, VarOnionOptinRequired),
+		WithExpiry(0),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygshp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqfp4qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q9qrsgq9vlvyj8cqvq6ggvpwd53jncp9nwc47xlrsnenq2zp70fq83qlgesn4u3uyf4tesfkkwwfg3qs54qe426hp3tz7z6sweqdjg05axsrjqp9yrrwc",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_010(t *testing.T) {
+	// On mainnet, with fallback (P2TR) address
+	// bc1pptdvg0d2nj99568qn6ssdy4cygnwuxgw2ukmnwgwz7jpqjz2kszse2s3lm
+
+	t.Skip("tagged field f not supported yet")
+
+	assert := assert.New(t)
+
+	pr := NewPaymentRequest(
+		2_000_000_000,
+		WithTimestamp(timestamp),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithDescriptionHash(longDescriptionHash),
+		WithFallbackAddress("bc1pptdvg0d2nj99568qn6ssdy4cygnwuxgw2ukmnwgwz7jpqjz2kszse2s3lm"), // P2TR
+		WithFeatureBits(PaymentSecretRequired, VarOnionOptinRequired),
+		WithExpiry(0),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqhp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqsfp4pptdvg0d2nj99568qn6ssdy4cygnwuxgw2ukmnwgwz7jpqjz2kszs9qrsgqy606dznq28exnydt2r4c29y56xjtn3sk4mhgjtl4pg2y4ar3249rq4ajlmj9jy8zvlzw7cr8mggqzm842xfr0v72rswzq9xvr4hknfsqwmn6xd",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_011(t *testing.T) {
+	// Please send 0.00967878534 BTC for a list of items
+	// within one week, amount in pico-BTC
+
+	t.Skip("tagged fields c and r not supported yet")
+
+	assert := assert.New(t)
+
+	paymentHashBytes, _ := hex.DecodeString("462264ede7e14047e9b249da94fefc47f41f7d02ee9b091815a5506bc8abf75f")
+
+	pr := NewPaymentRequest(
+		967_878_534,
+		WithTimestamp(time.Unix(1572468703, 0)),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithDescription("Blockstream Store: 88.85 USD for Blockstream Ledger Nano S x 1, \"Back In My Day\" Sticker x 2, \"I Got Lightning Working\" Sticker x 2 and 1 more items"),
+		WithExpiry(604800*time.Second),
+		// TODO: add min_final_cltv_expiry_delta (c) field from test vector
+		// TODO: add routing info (r) from test vector
+		WithFeatureBits(PaymentSecretRequired, VarOnionOptinRequired),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc9678785340p1pwmna7lpp5gc3xfm08u9qy06djf8dfflhugl6p7lgza6dsjxq454gxhj9t7a0sd8dgfkx7cmtwd68yetpd5s9xar0wfjn5gpc8qhrsdfq24f5ggrxdaezqsnvda3kkum5wfjkzmfqf3jkgem9wgsyuctwdus9xgrcyqcjcgpzgfskx6eqf9hzqnteypzxz7fzypfhg6trddjhygrcyqezcgpzfysywmm5ypxxjemgw3hxjmn8yptk7untd9hxwg3q2d6xjcmtv4ezq7pqxgsxzmnyyqcjqmt0wfjjq6t5v4khxsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygsxqyjw5qcqp2rzjq0gxwkzc8w6323m55m4jyxcjwmy7stt9hwkwe2qxmy8zpsgg7jcuwz87fcqqeuqqqyqqqqlgqqqqn3qq9q9qrsgqrvgkpnmps664wgkp43l22qsgdw4ve24aca4nymnxddlnp8vh9v2sdxlu5ywdxefsfvm0fq3sesf08uf6q9a2ke0hc9j6z6wlxg5z5kqpu2v9wz",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_012(t *testing.T) {
+	// Please send $30 for coffee beans to the same peer,
+	// which supports features 8, 14 and 99, using secret
+	// 0x1111111111111111111111111111111111111111111111111111111111111111
+
+	t.Skip("tagged field r and c not supported yet")
+
+	assert := assert.New(t)
+
+	pr := NewPaymentRequest(
+		2_500_000_000,
+		WithTimestamp(time.Unix(1572468703, 0)),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithDescription("coffee beans"),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		// TODO: add min_final_cltv_expiry_delta (c) field from test vector
+		// TODO: add routing info (r) from test vector
+		WithFeatureBits(99, PaymentSecretRequired, VarOnionOptinRequired),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc9678785340p1pwmna7lpp5gc3xfm08u9qy06djf8dfflhugl6p7lgza6dsjxq454gxhj9t7a0sd8dgfkx7cmtwd68yetpd5s9xar0wfjn5gpc8qhrsdfq24f5ggrxdaezqsnvda3kkum5wfjkzmfqf3jkgem9wgsyuctwdus9xgrcyqcjcgpzgfskx6eqf9hzqnteypzxz7fzypfhg6trddjhygrcyqezcgpzfysywmm5ypxxjemgw3hxjmn8yptk7untd9hxwg3q2d6xjcmtv4ezq7pqxgsxzmnyyqcjqmt0wfjjq6t5v4khxsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygsxqyjw5qcqp2rzjq0gxwkzc8w6323m55m4jyxcjwmy7stt9hwkwe2qxmy8zpsgg7jcuwz87fcqqeuqqqyqqqqlgqqqqn3qq9q9qrsgqrvgkpnmps664wgkp43l22qsgdw4ve24aca4nymnxddlnp8vh9v2sdxlu5ywdxefsfvm0fq3sesf08uf6q9a2ke0hc9j6z6wlxg5z5kqpu2v9wz",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_DecodeBech32_Spec_013(t *testing.T) {
+	// Same, but all upper case.
+
+	// LNBC25M1PVJLUEZPP5QQQSYQCYQ5RQWZQFQQQSYQCYQ5RQWZQFQQQSYQCYQ5RQWZQFQYPQDQ5VDHKVEN9V5SXYETPDEESSP5ZYG3ZYG3ZYG3ZYG3ZYG3ZYG3ZYG3ZYG3ZYG3ZYG3ZYG3ZYG3ZYGS9Q5SQQQQQQQQQQQQQQQQSGQ2A25DXL5HRNTDTN6ZVYDT7D66HYZSYHQS4WDYNAVYS42XGL6SGX9C4G7ME86A27T07MDTFRY458RTJR0V92CNMSWPSJSCGT2VCSE3SGPZ3UAPA
+
+	t.Skip("decode not supported yet")
+}
+
+func TestPaymentRequest_DecodeBech32_Spec_014(t *testing.T) {
+	// Same, but including fields which must be ignored.
+
+	// lnbc25m1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdq5vdhkven9v5sxyetpdeessp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygs9q5sqqqqqqqqqqqqqqqqsgq2qrqqqfppnqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqppnqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqpp4qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhpnqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhp4qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqspnqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqsp4qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqnp5qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqnpkqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqz599y53s3ujmcfjp5xrdap68qxymkqphwsexhmhr8wdz5usdzkzrse33chw6dlp3jhuhge9ley7j2ayx36kawe7kmgg8sv5ugdyusdcqzn8z9x
+
+	t.Skip("decode not supported yet")
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_015(t *testing.T) {
+	// Please send 0.01 BTC with payment metadata 0x01fafaf0
+
+	t.Skip("tagged field m not supported yet")
+
+	assert := assert.New(t)
+
+	paymentHashBytes, _ := hex.DecodeString("462264ede7e14047e9b249da94fefc47f41f7d02ee9b091815a5506bc8abf75f")
+
+	pr := NewPaymentRequest(
+		1_000_000_000,
+		WithTimestamp(timestamp),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithDescription("payment metadata inside"),
+		// TODO: add payment metadata (m) field from test vector
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithFeatureBits(48, PaymentSecretRequired, VarOnionOptinRequired),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc10m1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdp9wpshjmt9de6zqmt9w3skgct5vysxjmnnd9jx2mq8q8a04uqsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygs9q2gqqqqqqsgq7hf8he7ecf7n4ffphs6awl9t6676rrclv9ckg3d3ncn7fct63p6s365duk5wrk202cfy3aj5xnnp5gs3vrdvruverwwq7yzhkf5a3xqpd05wjc",
+		encoded,
+	)
+}
+
+func TestPaymentRequest_EncodeBech32_Spec_016(t *testing.T) {
+	// Public-key recovery with high-S signature
+
+	t.Skip("not sure why this doesn't work yet")
+
+	assert := assert.New(t)
+
+	pr := NewPaymentRequest(
+		0,
+		WithTimestamp(timestamp),
+		WithPaymentSecret([32]byte(paymentSecretBytes)),
+		WithPaymentHash([32]byte(paymentHashBytes)),
+		WithDescription("Please consider supporting this project"),
+		WithFeatureBits(PaymentSecretRequired, VarOnionOptinRequired),
+		WithExpiry(0),
+	)
+
+	encoded, err := pr.EncodeBech32(&TestSigner{})
+
+	assert.NoError(err)
+	assert.Equal(
+		"lnbc1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdpl2pkx2ctnv5sxxmmwwd5kgetjypeh2ursdae8g6twvus8g6rfwvs8qun0dfjkxaq9qrsgq357wnc5r2ueh7ck6q93dj32dlqnls087fxdwk8qakdyafkq3yap2r09nt4ndd0unm3z9u5t48y6ucv4r5sg7lk98c77ctvjczkspk5qprc90gx",
 		encoded,
 	)
 }
