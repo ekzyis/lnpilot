8f60de2abcaf0c8b3a5e9c38f7742d305df757ec
ekzyis <ramdip.singhgill@gmail.com>
2025-12-18 16:59:30 +0100

    Move bolt11 encoders into own file

--- lightning/bolt11/bolt11.go
+++ lightning/bolt11/bolt11.go
@@ -89,22 +89,6 @@ const (
 	MaxDescriptionBytes = 639
 )
 
-type Bolt11Encoder interface {
-	EncodeBolt11() ([]byte, error)
-}
-
-type StringBolt11Encoder struct {
-	*bech32.BytesBase32Encoder
-}
-
-type VarUintBolt11Encoder struct {
-	*bech32.VarUintBase32Encoder
-}
-
-type UintBolt11Encoder struct {
-	*bech32.UintBase32Encoder
-}
-
 func NewPaymentRequest(msats uint64, options ...func(*PaymentRequest)) *PaymentRequest {
 	var paymentSecret lntypes.Hash
 	// rand.Read never returns an error, and always fills the buffer entirely
@@ -442,31 +426,4 @@ func (pr *PaymentRequest) sign(signer secp256k1.Signer, buf *bytes.Buffer, hrp s
 	buf.Write(sigBase32)
 
 	return nil
-}
-
-func (e StringBolt11Encoder) EncodeBolt11() ([]byte, error) {
-	return e.EncodeBase32()
-}
-
-func (e VarUintBolt11Encoder) EncodeBolt11() ([]byte, error) {
-	return e.EncodeBase32()
-}
-
-func (e UintBolt11Encoder) EncodeBolt11() ([]byte, error) {
-	return e.EncodeBase32()
-}
-
-func NewStringBolt11Encoder(data string) Bolt11Encoder {
-	encoder := bech32.NewStringBase32Encoder(data)
-	return StringBolt11Encoder{BytesBase32Encoder: &encoder}
-}
-
-func NewUintBolt11Encoder(num, bitLen uint) Bolt11Encoder {
-	encoder := bech32.NewUintBase32Encoder(num, bitLen)
-	return UintBolt11Encoder{UintBase32Encoder: &encoder}
-}
-
-func NewVarUintBolt11Encoder(num uint) Bolt11Encoder {
-	encoder := bech32.NewVarUintBase32Encoder(num)
-	return VarUintBolt11Encoder{VarUintBase32Encoder: &encoder}
 }
---
+++ lightning/bolt11/encoding.go
@@ -0,0 +1,46 @@
+package bolt11
+
+import "github.com/ekzyis/lntutor/lib/bech32"
+
+type Bolt11Encoder interface {
+	EncodeBolt11() ([]byte, error)
+}
+
+type StringBolt11Encoder struct {
+	*bech32.BytesBase32Encoder
+}
+
+type VarUintBolt11Encoder struct {
+	*bech32.VarUintBase32Encoder
+}
+
+type UintBolt11Encoder struct {
+	*bech32.UintBase32Encoder
+}
+
+func (e StringBolt11Encoder) EncodeBolt11() ([]byte, error) {
+	return e.EncodeBase32()
+}
+
+func (e VarUintBolt11Encoder) EncodeBolt11() ([]byte, error) {
+	return e.EncodeBase32()
+}
+
+func (e UintBolt11Encoder) EncodeBolt11() ([]byte, error) {
+	return e.EncodeBase32()
+}
+
+func NewStringBolt11Encoder(data string) Bolt11Encoder {
+	encoder := bech32.NewStringBase32Encoder(data)
+	return StringBolt11Encoder{BytesBase32Encoder: &encoder}
+}
+
+func NewUintBolt11Encoder(num, bitLen uint) Bolt11Encoder {
+	encoder := bech32.NewUintBase32Encoder(num, bitLen)
+	return UintBolt11Encoder{UintBase32Encoder: &encoder}
+}
+
+func NewVarUintBolt11Encoder(num uint) Bolt11Encoder {
+	encoder := bech32.NewVarUintBase32Encoder(num)
+	return VarUintBolt11Encoder{VarUintBase32Encoder: &encoder}
+}
