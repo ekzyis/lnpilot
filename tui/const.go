3398751106d42cd9fb6e051374a10a3a14922752
ekzyis <ramdip.singhgill@gmail.com>
2026-01-02 22:07:16 +0100

    Simple menu

---
+++ tui/const.go
@@ -0,0 +1,13 @@
+package tui
+
+import "github.com/charmbracelet/lipgloss"
+
+const (
+	lightningColor = lipgloss.Color("#fada5e")
+	mutedColor     = lipgloss.Color("#707070")
+	Banner         = "" +
+		`   __     __       __          ` + "\n" +
+		`  / /__  / /___ __/ /____  ____` + "\n" +
+		` / / _ \/ __/ // / __/ _ \/ __/` + "\n" +
+		`/_/_//_/\__/\_,_/\__/\___/_/   `
+)
--- tui/model.go
+++ tui/model.go
@@ -9,19 +9,15 @@ import (
 )
 
 type model struct {
-	spinner spinner.Model
-	screen  screen
+	spinner  spinner.Model
+	screen   screen
+	selected int
 }
 
 type screen struct {
 	width  int
 	height int
 }
-
-var (
-	lightningColor = lipgloss.Color("#fada5e")
-	mutedColor     = lipgloss.Color("#707070")
-)
 
 func initialModel() model {
 	s := spinner.New()
@@ -37,21 +33,50 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.screen = screen{width: msg.Width, height: msg.Height}
-		return m, nil
+		return m.updateScreen(msg)
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "esc", "ctrl+c":
-			return m, tea.Quit
+			return m.quit()
+		case "up", "k":
+			return m.selectPrevious()
+		case "down", "j":
+			return m.selectNext()
 		default:
 			return m, nil
 		}
-
 	default:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
+		return m.updateSpinner(msg)
 	}
+}
+
+func (m *model) updateScreen(msg tea.WindowSizeMsg) (tea.Model, tea.Cmd) {
+	m.screen = screen{width: msg.Width, height: msg.Height}
+	return m, nil
+}
+
+func (m *model) updateSpinner(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.spinner, cmd = m.spinner.Update(msg)
+	return m, cmd
+}
+
+func (m *model) quit() (tea.Model, tea.Cmd) {
+	return m, tea.Quit
+}
+
+func (m *model) selectPrevious() (tea.Model, tea.Cmd) {
+	if m.selected > 0 {
+		m.selected--
+	}
+	return m, nil
+}
+
+func (m *model) selectNext() (tea.Model, tea.Cmd) {
+	if m.selected < 1 {
+		m.selected++
+	}
+	return m, nil
 }
 
 func (m model) View() string {
@@ -61,10 +86,35 @@ func (m model) View() string {
 		AlignHorizontal(lipgloss.Center).
 		AlignVertical(lipgloss.Center).
 		Render(
-			fmt.Sprintf("%s setting up your lightning node ...\n", m.spinner.View()),
+			fmt.Sprintf("%s\n", Banner),
+			menu(m.selected),
 			lipgloss.NewStyle().
 				Padding(1).
 				Foreground(mutedColor).
 				Render("press q to quit"),
+		)
+}
+
+func menu(selected int) string {
+	buttons := []string{"New Node", "Load Node"}
+	buttonStyle := lipgloss.NewStyle().Margin(1, 0)
+
+	buttonViews := make([]string, len(buttons))
+	for i, btn := range buttons {
+		if i == selected {
+			buttonViews[i] = buttonStyle.Bold(true).Render(">", btn)
+		} else {
+			buttonViews[i] = buttonStyle.Render(" ", btn)
+		}
+	}
+
+	return lipgloss.NewStyle().
+		Padding(1).
+		Align(lipgloss.Center).
+		Render(
+			lipgloss.JoinVertical(
+				lipgloss.Left,
+				buttonViews...,
+			),
 		)
 }
