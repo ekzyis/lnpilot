1382fee04a9b3784b6cb47f6f48ff5bb25d7470e
ekzyis <ramdip.singhgill@gmail.com>
2026-01-04 09:56:31 +0100

    Add logf as log primitive

---
+++ tui/log.go
@@ -0,0 +1,27 @@
+package tui
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+var (
+	f *os.File
+)
+
+func init() {
+	var err error
+	f, err = tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatalf("error opening debug.log: %v", err)
+	}
+}
+
+func logf(format string, a ...any) {
+	if f != nil {
+		fmt.Fprintf(f, format, a...)
+	}
+}
